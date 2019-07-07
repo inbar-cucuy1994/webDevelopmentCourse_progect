4f299e6e69ecbb17064230d5fbd44e485f534a94
inbar-cucuy1994 <50276626+inbar-cucuy1994@users.noreply.github.com>
2019-07-07 04:20:58 +0300

    Add files via upload

---
+++ meat_final/jScripts/JavaScript.js
@@ -0,0 +1,72 @@
+﻿/* Demo purposes only */
+$(".hover").mouseleave(
+    function () {
+        $(this).removeClass("hover");
+    }
+);
+
+//של קרן
+/* Demo purposes only */
+$(".hover").mouseleave(
+    function () {
+        $(this).removeClass("hover");
+    }
+);
+
+//$("#sec2 ul li").click(function () {
+//    console.log("1");
+//    })
+
+function myFunction(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg");
+    // Get the image text
+    var imgText = document.getElementById("imgtext");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+}
+function myFunction1(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg1");
+    // Get the image text
+    var imgText = document.getElementById("imgtext1");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+}
+
+//tab-key
+function tab_nav() {
+    var navA = document.querySelectorAll("ul li>a");
+    var navInner = document.querySelectorAll("li ul li>a");
+
+    for (var i = 0, len = navA.length; i < len; i++) {
+        navA[i].addEventListener("focus", function (event) {
+            var innerUl = event.target.parentElement.querySelector("ul");
+            event.target.parentElement.parentElement.querySelector("ul").style.display = "none";
+            if (innerUl) innerUl.style.display = "block";
+        }, true);
+
+        navA[i].addEventListener("click", function (event) {
+            event.target.parentElement.querySelector("ul").style.display = "none";
+
+        });
+
+    }
+
+    for (var i = 0, len = navInner.length; i < len; i++) {
+        if (navInner[i].parentElement.nextElementSibling === null || navInner[i].parentElement.previouselementsibling === null) {
+            navInner[i].addEventListener("blur", function (event) {
+                event.target.parentElement.parentElement.style.display = "none"
+            }, true);
+        }
+    }
+
+}
